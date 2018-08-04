8abe3f51896597f9df64adad027c678290af29fd
Leonardo Vitale <leonardo.vitale@outlook.com>
2018-08-04 16:41:01 -0300

    Create the Models of ORM using sequelize

--- slack-server/index.js
+++ slack-server/index.js
@@ -1,8 +1,10 @@
-import  express from 'express'
-import { ApolloServer, gql }  from 'apollo-server-express';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+
+import models from './models';
 
 const server = new ApolloServer({
   typeDefs,
@@ -12,14 +14,17 @@ const server = new ApolloServer({
     settings: {
       'editor.theme': 'light',
       'editor.cursorShape': 'line',
-      'editor.fontSize': 14
+      'editor.fontSize': 14,
     },
-  }
+  },
 });
 
 const app = express();
+
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`),
-);
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+
+models.sequelize.sync({ force: true }).then(() => {
+  app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`));
+});
