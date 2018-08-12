48443971e8a856b7bdd03a53cef73f31761a888c
Leonardo Vitale <leonardo.vitale@outlook.com>
2018-08-12 02:42:07 -0300

    [SERVER] - Criated Error Sqhema and update the Register Mutaion

--- slack-server/resolvers/user.js
+++ slack-server/resolvers/user.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import formatErrors from './utils';
 
 export default {
   Query: {
@@ -8,11 +9,29 @@ export default {
   Mutation: {
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
+        if (password.length < 5 || password.length > 100) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: 'password',
+                message: 'The password needs to be between 5 and 100 characters long',
+              },
+            ],
+          };
+        }
+
         const hashedPassword = await bcrypt.hash(password, 12);
-        await models.User.create({ ...otherArgs, password: hashedPassword });
-        return true;
+        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        return {
+          ok: true,
+          user,
+        };
       } catch (err) {
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err, models),
+        };
       }
     },
   },
